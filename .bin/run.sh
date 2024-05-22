a078a8b64e51c3c674c0e167f6c5f0d6bea3193e
elmorem <elmorem@gmail.com>
2024-05-22 13:08:04 -0700

    fix docker setup

--- .bin/run.sh
+++ .bin/run.sh
@@ -5,5 +5,6 @@ set -e
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
+pytest
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgin
---
+++ fowldata/fowldata/tests/test_basics.py
@@ -0,0 +1,5 @@
+import pytest
+
+def one_is_1(self):
+    assert 1 == 1
+    
