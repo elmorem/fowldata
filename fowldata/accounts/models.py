47c5c71768fb3c3bd97817989b875d555eddb68b
elmorem <elmorem@gmail.com>
2024-05-22 14:27:35 -0700

    add basic user model

--- fowldata/accounts/models.py
+++ fowldata/accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+class MyUserManager(BaseUserManager):
+    """MYUSER MANAGER"""
+
+    def self(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    objects = MyUserManager()
+
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default = False)
+
+    USERNAME_FIELD = "username"
