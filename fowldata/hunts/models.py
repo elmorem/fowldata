4bc7043eec1319bbd401d1fa0556e9de4c83e4e0
elmorem <elmorem@gmail.com>
2024-06-03 12:57:53 -0700

    add some models and weather data json

--- fowldata/hunts/models.py
+++ fowldata/hunts/models.py
@@ -12,3 +12,27 @@ class Hunt(models.Model):
     total_ducks = models.IntegerField(default=0, help_text=" Total number of ducks taken during hunt.")
     total_geese = models.IntegerField(default=0, help_text=" Total number of geese taken during hunt.")
     photo_url = models.URLField() 
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    #  weather = models.ForeignKey(Weather, on_delete=models.CASCADE)
+
+class WeatherManager(models.Manager):
+    pass
+
+class Weather (models.Model):
+    weatherID = models.BigAutoField(primary_key=True)
+    date = models.DateField()
+    tempmax = models.FloatField()
+    tempmin = models.FloatField()
+    temp = models.FloatField()
+    dew = models.FloatField()
+    humidity = models.FloatField()
+    precip = models.FloatField()
+    precipprob =models.FloatField()
+    precipcover = models.FloatField()
+    preciptype = models.CharField(max_length=100)
+    windgust = models.FloatField()
+    windspeed = models.FloatField()
+    winddir = models.FloatField()
+    sunrise = models.CharField(max_length=100)
+    sunset = models.CharField(max_length=100)
