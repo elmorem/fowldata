a4f75a8e3e068a55666e4f3d1ce8b7357232302c
elmorem <elmorem@gmail.com>
2024-06-13 18:02:32 -0700

    manual form

--- fowldata/hunts/forms.py
+++ fowldata/hunts/forms.py
@@ -5,7 +5,7 @@ from hunts.models import Hunt
 class CreateHuntForm(ModelForm):
     class Meta:
         model = Hunt
-        fields = "__all__"
+        fields = ['date_of_hunt', 'photo_url', 'location', 'total_ducks', 'total_geese', 'bird1', 'notes' ]
         labels = {
             'first_clue': 'First Clue',
         }
