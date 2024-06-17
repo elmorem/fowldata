497bd20c1343e90836f3b9de9e6f5d7887b72266
elmorem <elmorem@gmail.com>
2024-06-17 12:23:34 -0700

    changing form

--- fowldata/hunts/forms.py
+++ fowldata/hunts/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 
 from hunts.models import Hunt
 
@@ -8,4 +8,10 @@ class CreateHuntForm(ModelForm):
         fields = ['date_of_hunt', 'photo_url', 'location', 'total_ducks', 'total_geese', 'bird1', 'notes' ]
         labels = {
             'first_clue': 'First Clue',
+        }
+        widgets = {
+            "notes": Textarea(attrs={"cols": 80, "rows": 20}),
+        }
+        help_texts = {
+            'photo_url': "upload your photo here",
         }
