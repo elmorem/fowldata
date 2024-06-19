cac68a52f0c3ad412ccf6e0a5aef97190c73329a
Mark Elmore <elmorem@gmail.com>
2024-06-19 16:45:44 -0700

    feature hunt form (#6)
    * manual form
    
    * changing form
    
    * update requirements

--- fowldata/hunts/forms.py
+++ fowldata/hunts/forms.py
@@ -1,11 +1,15 @@
-from django.forms import ModelForm
+from mapwidgets.widgets import MapboxPointFieldWidget
+
+from django.forms import ModelForm, Textarea
 
 from hunts.models import Hunt
 
 class CreateHuntForm(ModelForm):
     class Meta:
         model = Hunt
-        fields = "__all__"
-        labels = {
-            'first_clue': 'First Clue',
+        fields = ['date_of_hunt', 'photo_url', 'location', 'total_ducks', 'total_geese', 'bird1', 'notes' ]
+        widgets = {
+            "notes": Textarea(attrs={"cols": 80, "rows": 20}),
+            'location': MapboxPointFieldWidget(),
+            'photo_url': "upload your photo here",
         }
