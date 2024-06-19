e5a214585803a162c499014f656cefab66448ddf
elmorem <elmorem@gmail.com>
2024-06-19 16:54:53 -0700

    add requirements

--- fowldata/hunts/forms.py
+++ fowldata/hunts/forms.py
@@ -1,15 +1,11 @@
-from mapwidgets.widgets import MapboxPointFieldWidget
-
 from django.forms import ModelForm, Textarea
 
 from hunts.models import Hunt
-
 class CreateHuntForm(ModelForm):
     class Meta:
         model = Hunt
         fields = ['date_of_hunt', 'photo_url', 'location', 'total_ducks', 'total_geese', 'bird1', 'notes' ]
-        widgets = {
-            "notes": Textarea(attrs={"cols": 80, "rows": 20}),
-            'location': MapboxPointFieldWidget(),
-            'photo_url': "upload your photo here",
-        }
+        # widgets = {
+        #     "notes": Textarea(attrs={"cols": 80, "rows": 20}),
+        #     'photo_url': "upload your photo here",
+        # }
