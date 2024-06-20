fd95e60a643ce4c3db847cb08572b1d92cd9b791
elmorem <elmorem@gmail.com>
2024-06-20 13:19:51 -0700

    add basic nav

--- fowldata/hunts/forms.py
+++ fowldata/hunts/forms.py
@@ -1,15 +1,19 @@
-from mapwidgets.widgets import MapboxPointFieldWidget
-
-from django.forms import ModelForm, Textarea
+from django import forms
+from django.forms import ModelForm
 
 from hunts.models import Hunt
 
 class CreateHuntForm(ModelForm):
     class Meta:
         model = Hunt
-        fields = ['date_of_hunt', 'photo_url', 'location', 'total_ducks', 'total_geese', 'bird1', 'notes' ]
-        widgets = {
-            "notes": Textarea(attrs={"cols": 80, "rows": 20}),
-            'location': MapboxPointFieldWidget(),
-            'photo_url': "upload your photo here",
-        }
+        fields = "__all__"
+        # ['date_of_hunt',  'latitude', 'longitude', 'total_ducks', 'total_geese', 'bird1', 'notes' ]
+        # widgets = {
+        #     'date_of_hunt': forms.SelectDateWidget(),
+        #    'latitude': forms.NumberInput(),
+        #    'longitude': forms.NumberInput(),
+        #     'total_ducks': forms.NumberInput(),
+        #     'total_geese': forms.NumberInput(),
+        #     'bird1': forms.Select(choices=Hunt.WATERFOWL_CHOICES),
+        #     "notes": forms.Textarea(attrs={"cols": 80, "rows": 20}),
+        # }
--- fowldata/hunts/views.py
+++ fowldata/hunts/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.conf import settings
 
@@ -14,15 +16,31 @@ def get_hunt(request):
 def update_hunt(request):
     pass
 
-
-def create_hunt(request):
+def create_new_hunt(request):
     context = {
-            'user_id': request.user.id,
-            'MAPBOX_ACCESS_TOKEN': settings.MAPBOX_ACCESS_TOKEN,
+        'user_id': request.user.id,
+        'MAPBOX_ACCESS_TOKEN': settings.MAPBOX_ACCESS_TOKEN,
     }
     if request.method == 'POST':
         form = CreateHuntForm(request.POST)
         context['form'] = form
         if form.is_valid():
             form.save()
+    print(context)
+    return render(request, 'hunts/_create_new_hunt.html', context)
+@login_required
+def create_hunt(request):
+    context = {
+            'user_id': request.user.id,
+            'MAPBOX_ACCESS_TOKEN': settings.MAPBOX_ACCESS_TOKEN,
+    }
+    if request.method == 'POST':
+        form = CreateHuntForm() # does this need to get a request.POST?
+        context['form'] = form
+        print(f"prointing context from within create_hunt: {context}")
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("hunts/thank_you/")
+    else:
+        form = CreateHuntForm()
     return render(request, 'hunts/create_hunt.html', context)
