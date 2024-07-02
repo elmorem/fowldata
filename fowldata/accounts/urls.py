3e392cd24563535e804bf387bf9cd06f11a6b9d1
Mark Elmore <elmorem@gmail.com>
2024-07-02 15:38:03 -0700

    logged in user nav (#10)

--- fowldata/accounts/urls.py
+++ fowldata/accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('signup/', views.signup, name="signup"),
     path('login/', views.login_view, name="login"),
     path('profile/', views.profile, name="profile"),
+    path('logout/', views.logout_view, name="logout"),
    # path('update_profile/', views.update_profile, name="update_profile"),
    # path('signup_thank_you/', views.signup_thank_you, name="signup_thank_you"),
 ]
